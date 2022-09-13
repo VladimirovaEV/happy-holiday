e41058d9d281fb421b9972926b3d10e17486f4eb
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-09-13 19:09:38 +0400

    project ready

--- src/const/const.js
+++ src/const/const.js
@@ -1,3 +1,5 @@
-// export const URI_API = 'https://vanilla-legend-factory.glitch.me/api/';
-export const URI_API = 'http://localhost:3000/api';
+export const URI_API = 'https://vanilla-legend-factory.glitch.me/api';
+// export const URI_API = 'http://localhost:3000/api';
 export const URI_PREFIX = "/api";
+// export const URI_API = 'https://boiling-bloom-blackbird.glitch.me'; 
+
